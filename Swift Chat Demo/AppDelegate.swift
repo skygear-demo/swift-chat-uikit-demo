c63a0580ca18b034522964f8307f28b53a0c2c22
Ben Lei <benlei@oursky.com>
2017-05-17 18:09:34 +0800

    WIP: enable push notification

--- Swift Chat Demo/AppDelegate.swift
+++ Swift Chat Demo/AppDelegate.swift
@@ -7,41 +7,71 @@
 //
 
 import UIKit
+import UserNotifications
 import SKYKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var skygear: SKYContainer {
+        return SKYContainer.default()!
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        SKYContainer.default().configAddress("http://localhost:3001")
-        SKYContainer.default().configure(withAPIKey: "my_skygear_key")
+        self.skygear.configAddress("http://localhost:3001")
+        self.skygear.configure(withAPIKey: "my_skygear_key")
+        self.skygear.registerDeviceCompletionHandler { (deviceID, error) in
+            guard error == nil else {
+                print("Got error when register device: \(error!.localizedDescription)")
+                return
+            }
+
+            if let id = deviceID {
+                print("Registered device with ID: \(id)")
+            } else {
+                print("Got nil device ID")
+            }
+        }
+
+        let notificationCneter = UNUserNotificationCenter.current()
+        notificationCneter.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            guard error == nil else {
+                print("Failed to authorize: \(error!.localizedDescription)")
+                return
+            }
+
+            guard granted else {
+                print("Authorization not granted")
+                return
+            }
+
+            application.registerForRemoteNotifications()
+        }
+
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        application.applicationIconBadgeNumber = 0
     }
 
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("Got remote notification device token")
+        self.skygear.registerRemoteNotificationDeviceToken(deviceToken) {(_, error) in
+            guard error == nil else {
+                print("Got error when register push notification token: \(error!.localizedDescription)")
+                return
+            }
+
+            print("Successfully registered push notification token")
+        }
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Fail to get remote notification device token: \(error.localizedDescription)")
     }
 
 }
